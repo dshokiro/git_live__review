7d7d246238b345cef0b13400f0eed23f8489840a
Doniyorbek <doniyorshokirov79@gmail.com>
2025-09-24 21:19:45 -0500

    Calculator code pushed

---
+++ src/day02__pull__clone__rollback/SimpleCalculator.java
@@ -0,0 +1,34 @@
+package day02__pull__clone__rollback;
+
+public class SimpleCalculator {
+
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int subtract(int a, int b) {
+        return a - b;
+    }
+
+    public static int multiply(int a, int b) {
+        return a * b;
+    }
+
+    public static double divide(int a, int b) {
+        if (b == 0) {
+            System.out.println("Cannot divide by zero.");
+            return 0;
+        }
+        return (double) a / b;
+    }
+
+    public static void main(String[] args) {
+        int x = 10, y = 5;
+        System.out.println("Add: " + add(x, y));
+        System.out.println("Subtract: " + subtract(x, y));
+        System.out.println("Multiply: " + multiply(x, y));
+        System.out.println("Divide: " + divide(x, y));
+
+    }
+
+}
